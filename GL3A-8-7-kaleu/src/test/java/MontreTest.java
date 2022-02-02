11a3c244e6b8558f1317aafab928c3eab5b71609
zeusclem <91337426+zeusclem@users.noreply.github.com>
2022-02-02 23:38:56 +0000

    Add files via upload

---
+++ GL3A-8-7-kaleu/src/test/java/MontreTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class MontreTest {
+
+
+}
